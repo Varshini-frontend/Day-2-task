540c263f4cf9d2e8a033d40c29c87c4d91c99db8
Varshini-frontend <varshinianandaraj@gmail.com>
2024-06-27 03:46:53 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,13 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " +petDog);
+console.log("My pet cat's name is: " +petCat);
+var catSound = "Purr";
+var catSound = "meow";
+var dogSound = "Woof";
+console.log(petDog + " says " + dogSound);
+console.log(petCat + " says " + catSound);
+console.log(petCat + " now says " + catSound);
+
